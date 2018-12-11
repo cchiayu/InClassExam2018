e43767d038711497d7b33fc6439295b15393d5c2
user <user@es302n30>
2018-12-11 11:56:49 +0800

    in class unfinished job

---
+++ src/com/exam/Order.java
@@ -0,0 +1,13 @@
+package com.exam;
+
+public class Order {
+	public int add(){
+		
+	
+	public print() {
+		
+	System.out.print(origin, destination, price, quantity);
+	
+	}
+}
+}
---
+++ src/com/exam/Ticket.java
@@ -0,0 +1,11 @@
+package com.exam;
+
+public class Ticket {
+	String origin;
+	String destination;
+	int price;
+	int quantity;
+	
+	ticket = new Ticket (origin, destination, price, quantity);
+	
+}
